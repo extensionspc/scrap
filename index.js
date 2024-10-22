89611d1f4e511ecc4c5c86dfff7e0700aeae4151
Muhammad <extensionspc@gmail.com>
2024-10-22 23:36:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const scrapeGoldPrices = async () => {
+const scrapeGoldPrices = async (req, res) => {
   try {
     const { data } = await axios.get('https://goldpriceqatar.com/');
     const $ = cheerio.load(data);
-
     const prices = [];
+
     $('table tr').each((index, element) => {
       const tds = $(element).find('td');
       prices.push({
@@ -16,10 +16,16 @@ const scrapeGoldPrices = async () => {
       });
     });
 
-    console.log(prices);
+    // Send the prices data as a simple HTML response
+    res.send(`
+      <h1>Gold Prices</h1>
+      <ul>
+        ${prices.map(price => `<li>${price.karat} - QAR: ${price.priceQAR}, USD: ${price.priceUSD}</li>`).join('')}
+      </ul>
+    `);
   } catch (error) {
-    console.error('Error scraping data:', error);
+    res.status(500).send('Error scraping data');
   }
 };
 
-scrapeGoldPrices();
+module.exports = scrapeGoldPrices;
