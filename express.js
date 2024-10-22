215a17d61de439efb9197f13fd5924568f80778c
Muhammad <extensionspc@gmail.com>
2024-10-22 23:38:38 +0500

    Update express.js

--- express.js
+++ express.js
@@ -1,1 +1,12 @@
-npm install express
+const express = require('express');
+const scrapeGoldPrices = require('./api/index'); // Import the scraping function
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Route to trigger the scraper
+app.get('/api', scrapeGoldPrices);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
